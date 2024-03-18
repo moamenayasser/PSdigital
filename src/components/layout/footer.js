854c3590cffe60c1eccee41d911fce58000178f4
moamenayasser <momena@paradigmsys.info>
2024-03-18 14:25:31 +0200

    225

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -87,8 +87,8 @@ const Footer = ({ footerData, socialData }) => {
               <span className="sr-only">Open main menu</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                width="15"
+                height="15"
                 viewBox="0 0 512 512"
               >
                 <g>
