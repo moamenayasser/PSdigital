01646401df83f4a6c6cb0fc324d2257256f43ad1
moamenayasser <momena@paradigmsys.info>
2024-06-12 17:30:25 +0300

    5.30

--- src/app/[locale]/[projectSlug]/innerProject/Conclusion.js
+++ src/app/[locale]/[projectSlug]/innerProject/Conclusion.js
@@ -7,6 +7,19 @@ const Conclusion = ({ data }) => {
       <motion.div
         initial={{ opacity: 0, y: 80 }}
         whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="w-full mb-8"
+      >
+        <img
+          src={data?.FeatureImageUrl}
+          alt={data?.Name}
+          className="rounded-[20px]
+              w-full h-[500px] object-cover"
+        />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 80 }}
+        whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, delay: 0.5 }}
         className="flex dark:text-white flex-col sm:flex-row text-[#001d35] rounded-[20px] mb-[30px] py-[43px] pr-[51px] pl-[49px] max-[767px]:px-[20px]"
       >
